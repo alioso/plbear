2e33180a0e00d4bdb1e5625a5539ec0c70b631f5
James Cole <jcole@zivtech.com>
2016-10-02 16:09:30 -0400

    Polyfill flexbox with flexibility

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-(function () {
+(function ($, Drupal) {
   'use strict';
-})();
+
+  Drupal.behaviors.polyfillFlexbox = {
+    attach: function (context, settings) {
+      $('body', context).once('polyfillFlexbox', function() {
+        if (flexibility instanceof Object) {
+          flexibility(document.body);
+        }
+      });
+    }
+  };
+
+})(jQuery, Drupal);
