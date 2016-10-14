fe68603be2a6c1418d40a74dcfb8b1c7574f6383
Alban Bailly <alban@zivtech.com>
2016-10-14 13:27:09 -0700

    simple mobile nav

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,27 @@
 
   Drupal.behaviors.polyfillFlexbox = {
     attach: function (context, settings) {
+
       $('body', context).once('polyfillFlexbox', function () {
         if (flexibility instanceof Object) {
           flexibility(document.body);
         }
       });
+
+    }
+  };
+
+  Drupal.behaviors.mobileNavigation = {
+    attach: function (context, settings) {
+
+      // define the navigation to be turned into a mobile one
+      var $mainMenu = $('.menu--main');
+
+      $('#mnav', context).on('click', function () {
+        $(this).toggleClass('open');
+        $mainMenu.toggleClass('open');
+      });
+
     }
   };
 
